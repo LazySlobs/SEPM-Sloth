ac5a8af231055db246763d9983054f5f7e752fae
baogia0912 <51979397+baogia0912@users.noreply.github.com>
2021-03-28 12:04:59 +0700

    added platform to differentiate between systems

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 # settings for global variables
+import platform, os
 
+platform = platform.system()
 energy_threshold = 2000 # change threshold to eliminate background noise
-location='/Users/bao/Desktop'
+location ='/Users/bao/Desktop'
 def init():
-    global energy_threshold, location
+    global energy_threshold, location, platform
