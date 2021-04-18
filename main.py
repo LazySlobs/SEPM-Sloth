b55f40ba06f5307777d6ac509bf756c362c12d0d
s3757317 <s3757317@users.noreply.github.com>
2021-04-19 01:35:43 +0700

    implemented keyword to wake the voice assistant

--- main.py
+++ main.py
@@ -11,14 +11,34 @@ r1 = sr.Recognizer() # create a recognizer object to recognize texts
 #r1.energy_threshold = settings.energy_threshold 
 r2 = sr.Recognizer() # create a recognizer object to respond
 
+WAKE = "hey sloth"
+
 def main():
-
-    voice_assistant_speak("How can I help you ?")
-
     while True:
-        voice_data, language = record_audio(r1)
-        print("Voice data: " + voice_data)
-        respond(r2, voice_data, language=language)
+        # speak if the ask variable is a string
+        with sr.Microphone() as source:
+            # starts to listen
+            print("Listening for keyword...")
+            audio = r1.listen(source)
+            listen_for_keyword = ""
+            try:
+                listen_for_keyword = r1.recognize_google(audio)
+            except sr.UnknownValueError:
+                continue
+            except sr.RequestError:
+                voice_assistant_speak("Sorry, my speech service is down")
+                continue
+            print("listen_for_keyword = " + listen_for_keyword)
+
+            if listen_for_keyword.count(WAKE) > 0:
+                print("Sloth is awake...")
+                voice_assistant_speak("How can I help you?")
+                print("Listening...")
+                voice_data, language = record_audio(r1)
+                print("Voice data: " + voice_data)
+                respond(r2, voice_data, language=language)
+            elif listen_for_keyword.lower() == "stop the program":
+                break
 
 if __name__ == '__main__':
     main()
