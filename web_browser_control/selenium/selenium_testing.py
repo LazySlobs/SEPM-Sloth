25f0f8aab7854d98df920f6ed5a4abd38fa63d65
koumi15cancer <s3757278@rmit.edu.vn>
2021-04-28 05:03:31 +0700

    (+) normal button, (+) href button, (+) label button

--- web_browser_control/selenium/selenium_testing.py
+++ web_browser_control/selenium/selenium_testing.py
@@ -7,16 +7,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+from selenium.webdriver.support.ui import Select
+
 path = "C:\Program Files (x86)\msedgedriver.exe"
 
-
+# Create Driver
 driver = webdriver.Edge(path)
 driver.maximize_window()
+
+#  Test Url
 url = "https://learn.letskodeit.com/p/practice"
 url1 = "https://en.wikipedia.org/wiki/Python_%28programming_language%29"
 url2 = "https://www.geeksforgeeks.org/deque-in-python/"
 url4 = "https://quotes.toscrape.com/"
-driver.get(url4)
+driver.get(url)
 
 
 
@@ -32,29 +36,66 @@ def highlight(element, effect_time, color, border):
     time.sleep(effect_time)
     apply_style(original_style)
 
-# open_window_elem = driver.find_element_by_xpath("//*[contains(text(), 'Mouse Hover')]")
+# open_window_elem = driver.find_element_by_xpath("//*[contains(text(), 'Open Window')]")
 # highlight(open_window_elem, 100
 #           , "red", 5)
 
 
 
-clickable_button = []
-elems = driver.find_elements_by_xpath("//a[@href]")
-# for elem in elems:
-#     print(elem.text)
-
-
-for elem in elems:
-    clickable_button.append(elem.text)
-
-
-print(clickable_button)
+# print(clickable_button)
 # buttons = driver.find_element_by_link_text("Hide").click()
 # driver.find_element_by_xpath("//*[contains(text(), 'object-oriented')]").click()
 
-button = input("Enter your button: ")
-if button in clickable_button:
-   print(button)
-   driver.find_element_by_xpath(f"//*[contains(text(), '{button}')]").click()
+# List of href button
+link_buttons = []
+links = driver.find_elements_by_xpath("//a[@href]")
+
+# Add label of href button to List
+for link in links:
+    link_buttons.append(link.text)
+
+# get href_button
+def href_button():
+ button = input("Enter your button: ")
+ if button in link_buttons:
+   try:
+     print(button)
+     driver.find_element_by_xpath(f"//*[contains(text(), '{button}')]").click()
+   except Exception:
+    print("Button not available")
 
 
+
+# List of label button
+label_buttons = []
+labels = driver.find_elements_by_tag_name("label")
+
+# Add label of href button to List
+for label in labels:
+    label_buttons.append(label.text)
+
+def label_button():
+ button = input("Enter your button: ")
+ if button in label_buttons:
+   try:
+     print(button)
+     driver.find_element_by_xpath(f"//label[@for='{button.lower()}')]").click()
+   except Exception:
+    print("Button not available")
+
+ # List of normal button
+normal_buttons = []
+buttons = driver.find_elements_by_xpath('//button')
+for button in buttons:
+    normal_buttons.append(button.text)
+
+def normal_button():
+ button = input("Enter your button: ")
+ if button in normal_buttons:
+   try:
+     print(button)
+     WebDriverWait(driver,1).until(EC.element_to_be_clickable((By.XPATH,f"//button[normalize-space()='{button}']"))).click()
+   except Exception:
+    print("Button not available")
+
+normal_button()
