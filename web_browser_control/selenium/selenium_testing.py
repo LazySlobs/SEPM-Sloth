a14b515b74c7328fe212f3811e1679849a47bea1
koumi15cancer <s3757278@rmit.edu.vn>
2021-04-28 08:46:54 +0700

    (+) add Place_holder button , (+) add Input_button

--- web_browser_control/selenium/selenium_testing.py
+++ web_browser_control/selenium/selenium_testing.py
@@ -46,16 +46,19 @@ def highlight(element, effect_time, color, border):
 # buttons = driver.find_element_by_link_text("Hide").click()
 # driver.find_element_by_xpath("//*[contains(text(), 'object-oriented')]").click()
 
-# List of href button
-link_buttons = []
-links = driver.find_elements_by_xpath("//a[@href]")
 
-# Add label of href button to List
-for link in links:
-    link_buttons.append(link.text)
 
 # get href_button
 def href_button():
+ # List of href button
+ link_buttons = []
+ links = driver.find_elements_by_xpath("//a[@href]")
+
+ # Add label of href button to List
+ for link in links:
+    link_buttons.append(link.text)
+
+ print(href_button())
  button = input("Enter your button: ")
  if button in link_buttons:
    try:
@@ -66,15 +69,19 @@ def href_button():
 
 
 
-# List of label button
-label_buttons = []
-labels = driver.find_elements_by_tag_name("label")
 
-# Add label of href button to List
-for label in labels:
+
+# get label_button
+def label_button():
+ # List of label button
+ label_buttons = []
+ labels = driver.find_elements_by_tag_name("label")
+
+ # Add label of href button to List
+ for label in labels:
     label_buttons.append(label.text)
 
-def label_button():
+ print(label_buttons)
  button = input("Enter your button: ")
  if button in label_buttons:
    try:
@@ -83,13 +90,18 @@ def label_button():
    except Exception:
     print("Button not available")
 
+
+
+
+# get normal_button
+def normal_button():
  # List of normal button
-normal_buttons = []
-buttons = driver.find_elements_by_xpath('//button')
-for button in buttons:
+ normal_buttons = []
+ buttons = driver.find_elements_by_xpath('//button')
+ for button in buttons:
     normal_buttons.append(button.text)
 
-def normal_button():
+ print(normal_buttons)
  button = input("Enter your button: ")
  if button in normal_buttons:
    try:
@@ -98,4 +110,46 @@ def normal_button():
    except Exception:
     print("Button not available")
 
-normal_button()
+
+
+
+
+#get input_button
+def input_button():
+
+ input_buttons = []
+ input_elements = driver.find_elements_by_xpath("//input[@value]")
+ for input_value in input_elements:
+    input_buttons.append(input_value.get_attribute('value'))
+
+ print(input_buttons)
+ button = input("Enter your button: ")
+ if button in input_buttons:
+     try:
+         print(button)
+         driver.find_element_by_xpath(f"//input[@value='{button}']").click()
+     except Exception:
+         print("Button not available")
+
+
+#get place_holder_button
+def place_holder_button():
+
+ place_holder_buttons = []
+ place_holder_elements = driver.find_elements_by_xpath("//input[@placeholder]")
+ for place_holder_value in place_holder_elements:
+    place_holder_buttons.append(place_holder_value.get_attribute('placeholder'))
+
+ print(place_holder_buttons)
+ button = input("Enter your button: ")
+ if button in place_holder_buttons:
+     try:
+         print(button)
+         driver.find_element_by_xpath(f"//input[@placeholder='{button}']").click()
+     except Exception:
+         print("Button not available")
+
+
+place_holder_button()
+
+
