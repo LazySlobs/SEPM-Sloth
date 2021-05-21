fa8654d9e53887bbe49a4124010a388917c5fdc2
s3757317 <s3757317@rmit.edu.vn>
2021-05-21 22:44:23 +0700

    converted to class object for web command functions

--- web_browser_control/web_command.py
+++ web_browser_control/web_command.py
@@ -1,10 +1,12 @@
-import pyautogui, pywinauto
+import pyautogui, pywinauto, time
+import settings
+import speech_recognition as sr
+from core.listen import record_audio
 
 #SELENIUM
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-import time
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -12,292 +14,236 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-import settings
-import speech_recognition as sr
-from core.listen import record_audio
+class Chrome:
+    # Initializing
+    def __init__(self):
+        pass
 
+    # Deleting (Calling destructor)
+    def __del__(self):
+        print('Chrome object destroyed.')
+    
+    def open_browser(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.original_window = self.driver.current_window_handle
+        self.windows = self.driver.window_handles
+        self.driver.maximize_window()
+        self.driver.get('https://www.google.com')
+        print(self.list_button())
 
-# FUNCTIONS
-def focus_browser():
-    try:
-        app = pywinauto.Application().connect(title_re=".*Chrome.*", found_index=0) # connect to chrome browser
-        browser = app.top_window()  # get browser at the top index
-        # browser.print_control_identifiers()   # print control identifiers for debug
-        browser.set_focus()  # bring browser window to front
-    except:
-        print("Can't find Chrome browser window.")
+    def focus_browser(self):
+        try:
+            app = pywinauto.Application().connect(title_re=".*Chrome.*", found_index=0) # connect to chrome browser
+            browser = app.top_window()  # get browser at the top index
+            # browser.print_control_identifiers()   # print control identifiers for debug
+            browser.set_focus()  # bring browser window to front
+        except:
+            print("Can't find Chrome browser window.")
 
-def go_to(driver, url):
-    driver.get(url)
+    def switch_to_window(self):
+        for window_handle in self.windows :
+            if window_handle != self.original_window:
+                self.driver.switch_to.window(window_handle)
 
-def list_button(driver):
-    clickable_button = []
-    elems = driver.find_elements_by_xpath("//a[@href]")
-    for elem in elems:
-        clickable_button.append(elem.text)
-    return clickable_button
+    def go_to(self, url):
+        self.driver.get(url)
 
-def select_button(driver, button):
-    try:
-        driver.find_element_by_xpath(f"//*[contains(text(), '{button}')]").click()
-    except:
-        print("Unable to click.")
+    def list_button(self):
+        clickable_button = []
+        elems = self.driver.find_elements_by_xpath("//a[@href]")
+        for elem in elems:
+            clickable_button.append(elem.text)
+        return clickable_button
 
-def refesh(driver):
-    driver.refresh()
+    def select_button(self, voice_data):
+        for elm in self.list_button():
+            print(elm)
+            if voice_data.lower() == elm.lower():
+                try:
+                    self.driver.find_element_by_xpath(f"//*[contains(text(), '{elm}')]").click()
+                    return True
+                except:
+                    print("Unable to select.")
+                    return False
+        print("Can't find element with matching string.")
+        return False
 
-def back(driver):
-    driver.back()
+    def refesh(self):
+        self.driver.refresh()
 
-def forward(driver):
-    driver.forward()
+    def back(self):
+        self.driver.back()
 
-def return_home(driver):
-    url = 'https://www.google.com'
-    driver.get(url)
+    def forward(self):
+        self.driver.forward()
 
-def minimize(driver):
-    driver.minimize_window()
+    def return_home(self):
+        url = 'https://www.google.com'
+        self.driver.get(url)
 
-def maximize(driver):
-    driver.maximize_window()
+    def minimize(self):
+        self.driver.minimize_window()
 
-def close_app(driver):
-    driver.quit()
+    def maximize(self):
+        self.driver.maximize_window()
 
-def full_screen(driver):
-    driver.fullscreen_window()
+    def close_window(self):
+        self.driver.close()
 
-def new_tab():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "t")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "t")
+    def close_browser(self):
+        self.driver.quit()
 
-def new_browser_window():
-    focus_browser()
+    def full_screen(self):
+        self.driver.fullscreen_window()
+
+    def new_tab(self):
+        self.focus_browser()
         # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl","n")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "n")
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "t")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "t")
 
-def open_history():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "h")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "y")
+    def new_browser_window(self):
+        self.focus_browser()
+            # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl","n")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "n")
 
-def select_address_bar():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "l")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "l")
+    def open_history(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "h")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "y")
 
-def scroll_to_top():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("home")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "up")
+    def select_address_bar(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "l")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "l")
 
-def scroll_to_bottom():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("end")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "down")
+    def scroll_to_top(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("home")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "up")
 
-def scroll_down():
-    focus_browser()
-    pyautogui.hotkey("space")
+    def scroll_to_bottom(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("end")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "down")
 
-def scroll_up():
-    focus_browser()
-    pyautogui.hotkey("shift", "space")
+    def scroll_down(self):
+        self.focus_browser()
+        pyautogui.hotkey("space")
 
-def book_mark_page():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "d")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "shift", "d")
+    def scroll_up(self):
+        self.focus_browser()
+        pyautogui.hotkey("shift", "space")
 
-def book_mark_list():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "shift", "o")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "shift", "l")
+    def book_mark_page(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "d")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "shift", "d")
 
-def private_window():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "shift", "n")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "shift", "n")
+    def book_mark_list(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "shift", "o")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "shift", "l")
 
-def text_search():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "f")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "f")
+    def private_window(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "shift", "n")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "shift", "n")
 
-def open_download_history():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "j")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("command", "option", "l")
+    def text_search(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "f")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "f")
 
-def clear_browsing_data():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "shift", "delete")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("option", "command", "e")
+    def open_download_history(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "j")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("command", "option", "l")
 
-def inspect_website():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "shift", "i")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("option", "command", "i")
+    def clear_browsing_data(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "shift", "delete")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("option", "command", "e")
 
-def next_tab():
-    focus_browser()
-    # try to open with os
-    if settings.platform == 'Windows':
-        pyautogui.hotkey("ctrl", "tab")
-    elif settings.platform == 'Darwin':
-        pyautogui.hotkey("ctrl", "tab")
+    def inspect_website(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "shift", "i")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("option", "command", "i")
 
-def navigate_windows():
-    focus_browser()
-    if settings.platform == 'Windows':
-        pyautogui.keyDown("alt")
-        pyautogui.keyDown("tab")
-        move_button()
-        pyautogui.keyUp("alt")
-        pyautogui.keyUp("tab")
-    elif settings.platform == 'Darwin':
-        pyautogui.keyDown("alt")
-        pyautogui.keyDown("tab")
-        move_button()
-        pyautogui.keyUp("alt")
-        pyautogui.keyUp("tab")
+    def next_tab(self):
+        self.focus_browser()
+        # try to open with os
+        if settings.platform == 'Windows':
+            pyautogui.hotkey("ctrl", "tab")
+        elif settings.platform == 'Darwin':
+            pyautogui.hotkey("ctrl", "tab")
 
-def move_button():
-    r1 = sr.Recognizer()  # create a recognizer object to recognize texts
-    r1.energy_threshold = 4000
-    while True:
-        navigate_data, language = record_audio(r1)
-        print(navigate_data)
-        if "right" in navigate_data:
-            pyautogui.press("right")
-        elif "left" in navigate_data:
-            pyautogui.press("left")
-        elif "up" in navigate_data:
-            pyautogui.press("up")
-        elif "down" in navigate_data:
-            pyautogui.press("down")
-        elif "enter" in navigate_data:
-            pyautogui.press("enter")
-            break
+    def move_button(self, r):
+        while True:
+            # navigate_data = input("navigate data: ")
+            navigate_data, language = record_audio(r)
+            print(navigate_data)
+            if "right" in navigate_data:
+                pyautogui.press("right")
+            elif "left" in navigate_data:
+                pyautogui.press("left")
+            elif "up" in navigate_data:
+                pyautogui.press("up")
+            elif "down" in navigate_data:
+                pyautogui.press("down")
+            elif "enter" in navigate_data:
+                pyautogui.press("enter")
+                break
 
-def processor(driver):
-    while True:
-        print(list_button(driver))
-        voice_data = input("What can I do?")
-
-        if voice_data == "switch window" or voice_data == "change window":
-            navigate_windows()
-        elif "go to" in voice_data:
-            url = voice_data.replace('go to ','https://')
-            go_to(driver, url)
-        elif "refresh" in voice_data or voice_data == "refresh page":
-            refesh(driver)
-        elif "go back" in voice_data:
-            back(driver)
-        elif "go forward" in voice_data:
-            forward(driver)
-        elif "return home" in voice_data:
-            return_home(driver)
-        elif "address bar" in voice_data:
-            select_address_bar()
-        elif "fullscreen" in voice_data or "full screen" in voice_data:
-            full_screen()
-        elif voice_data == "scroll to top" or voice_data == "scroll to the top":
-            scroll_to_top()
-        elif voice_data == "scroll to bottom" or voice_data == "scroll to the bottom":
-            scroll_to_bottom()
-        elif "scroll up" in voice_data:
-            scroll_up()
-        elif "scroll down" in voice_data:
-            scroll_down()
-        elif "bookmark page" in voice_data:
-            book_mark_page()
-        elif "bookmark list" in voice_data:
-            book_mark_list()
-        elif "open" in voice_data and "private window" in voice_data:
-            private_window()
-        elif "find text" in voice_data:
-            text_search()
-        elif voice_data == "open history":
-            open_history()
-        elif "dowload" in voice_data:
-            open_download_history()
-        elif voice_data == "clear browsing data":
-            clear_browsing_data()
-        elif "inspect" in voice_data or voice_data == "inspect website":
-            inspect_website()
-        elif "new window" in voice_data or voice_data == "open new window":
-            new_browser_window()
-        elif voice_data == "new tab" or voice_data == "open new tab":
-            new_tab()
-        elif voice_data == "next tab" or voice_data == "go to next tab":
-            next_tab()
-        elif voice_data in list_button(driver):
-            select_button(driver, voice_data)
-        elif "close app" in voice_data:
-            close_app(driver)
-            break
-        else : 
-            print("invalid command")
-
-def open_browser_window():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    original_window = driver.current_window_handle
-    windows = driver.window_handles
-    driver.maximize_window()    
-    url = 'https://www.google.com'
-    driver.get(url)
-
-    while True:
-        for window_handle in windows :
-            if window_handle != original_window:
-                driver.switch_to.window(window_handle)
-            time.sleep(5)
-            processor(driver)
-
-
-
-
-
-
+    def navigate_windows(self):
+        self.focus_browser()
+        if settings.platform == 'Windows':
+            pyautogui.keyDown("alt")
+            pyautogui.keyDown("tab")
+            self.move_button()
+            pyautogui.keyUp("alt")
+            pyautogui.keyUp("tab")
+        elif settings.platform == 'Darwin':
+            pyautogui.keyDown("alt")
+            pyautogui.keyDown("tab")
+            self.move_button()
+            pyautogui.keyUp("alt")
+            pyautogui.keyUp("tab")
